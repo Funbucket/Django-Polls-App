f817d4adc697989416573db810198662cd90a802
HaeChang <outofchng@gmail.com>
2021-02-14 01:19:13 +0900

    Refactor views

--- polls/views.py
+++ polls/views.py
@@ -7,10 +7,10 @@ from django.http import HttpResponse, response
 
 
 def index(request):
-    # return HttpResponse("Hello, World")
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s"%question_id)
 def results(request, question_id):
